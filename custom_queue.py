114eb9f623dcf6b39fd6a6a14c83ca5308fdfef3
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-03-02 11:41:08 +0300

    add property

--- custom_queue.py
+++ custom_queue.py
@@ -3,14 +3,22 @@ from utils import *
 
 class Queue:
     def __init__(self, head=None, tail=None):
-        self.head = head
-        self.tail = tail
+        self.__head = head
+        self.__tail = tail
 
     def enqueue(self, value) -> None:
         new_node = Node(data=value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
+        if self.__head is None:
+            self.__head = new_node
+            self.__tail = new_node
         else:
-            self.tail.next_node = new_node
-            self.tail = new_node
+            self.__tail.next_node = new_node
+            self.__tail = new_node
+
+    @property
+    def head(self):
+        return self.__head
+
+    @property
+    def tail(self):
+        return self.__tail
